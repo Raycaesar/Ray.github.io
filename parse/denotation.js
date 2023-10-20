a353d54d9ae572afa50c07bf9a4b9c6a5d03bc07
Ray Drury <30977437+Raycaesar@users.noreply.github.com>
2023-10-20 14:07:44 +1300

    Add files via upload

---
+++ parse/denotation.js
@@ -0,0 +1,215 @@
+let Prop = [];
+
+function setPropSize() {
+    const size = parseInt(document.getElementById("propSize").value);
+    Prop = [];
+    if (size < 4) {
+        Prop = ['p', 'q', 'r'];
+    } else {
+        for (let i = 1; i <= size; i++) {
+            Prop.push(`p_${i}`);
+        }
+    }
+    document.getElementById("propOutput").innerText = `Prop = {${Prop.join(', ')}}`;
+}
+
+function powerSet(array) {
+    return array.reduce((subsets, value) => subsets.concat(subsets.map(set => [value, ...set])), [[]]);
+}
+
+function isWellFormedSimpleCheck(formula) {
+    const binaryOperators = ['&', '+', '>'];
+
+    let operatorCount = 0;
+    for (const operator of binaryOperators) {
+        operatorCount += (formula.match(new RegExp(`\\${operator}`, 'g')) || []).length;
+    }
+
+    const bracketPairsCount = (formula.match(/\(/g) || []).length;
+
+    return operatorCount === bracketPairsCount;
+}
+
+// Tokenizer
+function tokenize(formula) {
+    return formula.match(/~|\+|&|>|[a-z]_[0-9]+|[a-z]|[\(\)]/g);
+}
+
+
+// Recursive parser
+function parse(tokens) {
+    if (tokens.length === 0) throw new Error("Unexpected end of input");
+
+    let token = tokens.shift();
+    
+    if (token === '~') {
+        return {
+            type: 'negation',
+            subformula: parse(tokens)
+        };
+    } else if (token === '(') {
+        let left = parse(tokens);
+        
+        if (tokens.length === 0 || ['&', '+', '>'].indexOf(tokens[0]) === -1) {
+            throw new Error("Expected an operator");
+        }
+        
+        let operator = tokens.shift(); 
+        
+        let right = parse(tokens);
+        
+        if (tokens[0] !== ')') {
+            throw new Error("Expected a closing bracket");
+        }
+        
+        tokens.shift();  
+        return {
+            type: operator,
+            left: left,
+            right: right
+        };
+    } else if (Prop.includes(token)) {  // atom
+        return {
+            type: 'atom',
+            value: token
+        };
+    } else {
+        throw new Error(`Unexpected token: ${token}`);
+    }
+}
+
+
+
+function replaceWithDenotation(parsedFormula) {
+    if (!parsedFormula) throw new Error("Invalid or non-well-formed formula.");
+
+    switch (parsedFormula.type) {
+        case 'atom':
+            const denotation = atomDenotation(parsedFormula.value);
+            if (denotation.length === 0) return '{}';
+            return `{{${denotation.map(set => set.join(', ')).join('}, {')}}}`;
+            
+        case 'negation':
+            const innerDenotation = replaceWithDenotation(parsedFormula.subformula);
+            if (innerDenotation === '{}') return '{{}}'; // Handle negation of empty set
+            if (innerDenotation.startsWith("{{") && innerDenotation.endsWith("}}")) {
+                let setString = innerDenotation.slice(2, -2).split('}, {'); 
+                let setArray = setString.map(str => str.split(', ').filter(Boolean));
+                let complementSet = complementOfSet(setArray);
+                if (complementSet.length === 0) return '{}'; // Return {} if the complement set is empty
+                return `{{${complementSet.map(set => set.join(', ')).join('}, {')}}}`;
+            }
+            return `~${innerDenotation}`;
+
+        case '&':
+        case '+':
+            const leftDenotation = replaceWithDenotation(parsedFormula.left);
+            const rightDenotation = replaceWithDenotation(parsedFormula.right);
+
+            if (leftDenotation.startsWith("{{") && leftDenotation.endsWith("}}") &&
+                rightDenotation.startsWith("{{") && rightDenotation.endsWith("}}")) {
+                
+                let setA = leftDenotation.slice(2, -2).split('}, {').map(str => str.split(', ').filter(Boolean));
+                let setB = rightDenotation.slice(2, -2).split('}, {').map(str => str.split(', ').filter(Boolean));
+
+                let resultSet;
+                if (parsedFormula.type === '&') {
+                    resultSet = setIntersection(setA, setB);
+                } else { // '+'
+                    resultSet = setUnion(setA, setB);
+                }
+                
+                if (resultSet.length === 0) return '{}'; // Return {} if the result set is empty
+                return `{{${resultSet.map(set => set.join(', ')).join('}, {')}}}`;
+            }
+
+            return `(${leftDenotation} ${parsedFormula.type} ${rightDenotation})`;
+
+        case '>':
+            const notLeft = {
+                type: 'negation',
+                subformula: parsedFormula.left
+            };
+            const orRight = {
+                type: '+',
+                left: notLeft,
+                right: parsedFormula.right
+            };
+            return replaceWithDenotation(orRight);
+
+        default:
+            throw new Error("Invalid or non-well-formed formula.");
+    }
+}
+
+
+
+
+
+
+function setUnion(setA, setB) {
+    const union = [...setA];
+    for (const subset of setB) {
+        if (!union.some(item => arraysAreEqual(item, subset))) {
+            union.push(subset);
+        }
+    }
+    return union;
+}
+
+function setIntersection(setA, setB) {
+    return setA.filter(subsetA => setB.some(subsetB => arraysAreEqual(subsetA, subsetB)));
+}
+
+function arraysAreEqual(arr1, arr2) {
+    if (arr1.length !== arr2.length) return false;
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) return false;
+    }
+    return true;
+}
+
+
+
+function complementOfSet(set) {
+    let powerSetOfProp = powerSet(Prop);
+    return powerSetOfProp.filter(subset => !set.some(item => arraysAreEqual(item, subset)));
+
+    function arraysAreEqual(arr1, arr2) {
+        if (arr1.length !== arr2.length) return false;
+        for (let i = 0; i < arr1.length; i++) {
+            if (arr1[i] !== arr2[i]) return false;
+        }
+        return true;
+    }
+}
+
+
+
+function atomDenotation(atom) {
+    return powerSet(Prop).filter(subset => subset.includes(atom));
+}
+
+
+
+function displayDenotation() {
+    try {
+        const formula = document.getElementById("formulaInput").value;
+
+        if (!isWellFormedSimpleCheck(formula)) {
+            throw new Error("The formula is not well-formed!");
+        }
+
+        const parsed = parse(tokenize(formula));
+        let result = replaceWithDenotation(parsed);
+        document.getElementById("resultOutput").innerText = result;
+    } catch (error) {
+        alert(error.message);
+    }
+}
+
+
+
+
+
+/*for verification  (p+(~q&((r+~p)>~(p>~r))))*/
