ee4fc02aba176a3af9e6fbf762cf2bfdb38fd44d
Ray Drury <30977437+Raycaesar@users.noreply.github.com>
2023-10-26 00:47:01 +1300

    Add files via upload

--- parse/parse.js
+++ parse/parse.js
@@ -54,11 +54,11 @@ function setAgentFollowers() {
 
 // Display Followers
 function displayFollowers() {
-    let outputText = '';
+    let outputfollower = '';
     for (let agent in agentFollowers) {
-        outputText += `f(${agent}) = {${agentFollowers[agent].join(', ')}}\n`;
+        outputfollower += `f(${agent}) = {${agentFollowers[agent].join(', ')}}\n`;
     }
-    document.getElementById("followerOutput").innerText = outputText;
+    document.getElementById("followerOutput").innerText = outputfollower;
 }
 
 // ================================================
@@ -86,12 +86,12 @@ function setPropSize() {
 
 
 // ================================================
-// =============== FORMULA FUNCTIONS ==============
+// =============== message FUNCTIONS ==============
 // ================================================
 
 // Tokenizer
-function tokenize(formula) {
-    return formula.match(/~|\+|&|>|[a-z]_[0-9]+|[a-z]|[\(\)]/g);
+function tokenize(message) {
+    return message.match(/~|\+|&|>|[a-z]_[0-9]+|[a-z]|[\(\)]/g);
 }
 
 // Recursive parser
@@ -103,7 +103,7 @@ function parse(tokens) {
     if (token === '~') {
         return {
             type: 'negation',
-            subformula: parse(tokens)
+            submessage: parse(tokens)
         };
     } else if (token === '(') {
         let left = parse(tokens);
@@ -136,16 +136,16 @@ function parse(tokens) {
     }
 }
 
-//Formula Check
-function isWellFormedSimpleCheck(formula) {
+//message Check
+function isWellFormedSimpleCheck(message) {
     const binaryOperators = ['&', '+', '>'];
 
     let operatorCount = 0;
     for (const operator of binaryOperators) {
-        operatorCount += (formula.match(new RegExp(`\\${operator}`, 'g')) || []).length;
+        operatorCount += (message.match(new RegExp(`\\${operator}`, 'g')) || []).length;
     }
 
-    const bracketPairsCount = (formula.match(/\(/g) || []).length;
+    const bracketPairsCount = (message.match(/\(/g) || []).length;
 
     return operatorCount === bracketPairsCount;
 }
@@ -157,19 +157,20 @@ function isWellFormedSimpleCheck(formula) {
 // ===============================================
 
 
-//We obtain denotation by substiting formulas from atomic with sets.
+//We obtain denotation by substiting messages from atomic with sets.
 
-function replaceWithDenotation(parsedFormula) {
-    if (!parsedFormula) throw new Error("Invalid or non-well-formed formula.");
+function replaceWithDenotation(parsedmessage) {
+    console.log("replaceWithDenotation called with:", parsedmessage);
+    if (!parsedmessage) throw new Error("Invalid or non-well-formed message.");
 
-    switch (parsedFormula.type) {
+    switch (parsedmessage.type) {
         case 'atom':
-            const denotation = atomDenotation(parsedFormula.value);
+            const denotation = atomDenotation(parsedmessage.value);
             if (denotation.length === 0) return '{}';
             return `{{${denotation.map(set => set.join(', ')).join('}, {')}}}`;
             
         case 'negation':
-            const innerDenotation = replaceWithDenotation(parsedFormula.subformula);
+            const innerDenotation = replaceWithDenotation(parsedmessage.submessage);
             if (innerDenotation === '{}') return '{{}}'; // Handle negation of empty set
             if (innerDenotation.startsWith("{{") && innerDenotation.endsWith("}}")) {
                 let setString = innerDenotation.slice(2, -2).split('}, {'); 
@@ -182,8 +183,8 @@ function replaceWithDenotation(parsedFormula) {
 
         case '&':
         case '+':
-            const leftDenotation = replaceWithDenotation(parsedFormula.left);
-            const rightDenotation = replaceWithDenotation(parsedFormula.right);
+            const leftDenotation = replaceWithDenotation(parsedmessage.left);
+            const rightDenotation = replaceWithDenotation(parsedmessage.right);
 
             if (leftDenotation.startsWith("{{") && leftDenotation.endsWith("}}") &&
                 rightDenotation.startsWith("{{") && rightDenotation.endsWith("}}")) {
@@ -192,7 +193,7 @@ function replaceWithDenotation(parsedFormula) {
                 let setB = rightDenotation.slice(2, -2).split('}, {').map(str => str.split(', ').filter(Boolean));
 
                 let resultSet;
-                if (parsedFormula.type === '&') {
+                if (parsedmessage.type === '&') {
                     resultSet = setIntersection(setA, setB);
                 } else { // '+'
                     resultSet = setUnion(setA, setB);
@@ -202,22 +203,22 @@ function replaceWithDenotation(parsedFormula) {
                 return `{{${resultSet.map(set => set.join(', ')).join('}, {')}}}`;
             }
 
-            return `(${leftDenotation} ${parsedFormula.type} ${rightDenotation})`;
+            return `(${leftDenotation} ${parsedmessage.type} ${rightDenotation})`;
 
         case '>':
             const notLeft = {
                 type: 'negation',
-                subformula: parsedFormula.left
+                submessage: parsedmessage.left
             };
             const orRight = {
                 type: '+',
                 left: notLeft,
-                right: parsedFormula.right
+                right: parsedmessage.right
             };
             return replaceWithDenotation(orRight);
 
         default:
-            throw new Error("Invalid or non-well-formed formula.");
+            throw new Error("Invalid or non-well-formed message.");
     }
 }
 
@@ -225,13 +226,13 @@ function replaceWithDenotation(parsedFormula) {
 // Display Denotation
 function displayDenotation() {
     try {
-        const formula = document.getElementById("formulaInput").value;
+        const message = document.getElementById("messageInput").value;
 
-        if (!isWellFormedSimpleCheck(formula)) {
-            throw new Error("The formula is not well-formed!");
+        if (!isWellFormedSimpleCheck(message)) {
+            throw new Error("The message is not well-formed!");
         }
 
-        const parsed = parse(tokenize(formula));
+        const parsed = parse(tokenize(message));
         let result = replaceWithDenotation(parsed);
         document.getElementById("resultOutput").innerText = result;
     } catch (error) {
@@ -246,20 +247,20 @@ function displayDenotation() {
 function assignBelief() {
     try {
         
-        const formula = document.getElementById("beliefFormula").value;
+        const message = document.getElementById("beliefmessage").value;
 
-        if (!isWellFormedSimpleCheck(formula)) {
-            throw new Error("The formula is not well-formed!");
+        if (!isWellFormedSimpleCheck(message)) {
+            throw new Error("The message is not well-formed!");
         }
 
         const selectedAgent = document.getElementById("beliefAgent").value;
-        const parsed = parse(tokenize(formula));
+        const parsed = parse(tokenize(message));
         const denotationResult = replaceWithDenotation(parsed);
 
         // Check if the agent already has beliefs
         if (agentBeliefs[selectedAgent]) {
-            // Append new formula to the beliefs
-            agentBeliefs[selectedAgent].formulas.push(formula);
+            // Append new message to the beliefs
+            agentBeliefs[selectedAgent].messages.push(message);
             
             // Intersect the new denotation with the previous one
             let oldDenotation = agentBeliefs[selectedAgent].denotation.slice(2, -2).split('}, {').map(str => str.split(', ').filter(Boolean));
@@ -274,7 +275,7 @@ function assignBelief() {
         } else {
             // If no prior beliefs, initialize with the current one
             agentBeliefs[selectedAgent] = {
-                formulas: [formula],
+                messages: [message],
                 denotation: denotationResult
             };
         }
@@ -293,20 +294,38 @@ function displayAgentBeliefs() {
     for (let agent in agentBeliefs) {
         // Wrap agent's name in a span, color it, and make it bold using inline CSS
         const coloredAgentName = `<span style="color: ${agentColors[agent]}; font-weight: bold;">${agent}</span>`;
-        outputText += `${coloredAgentName} believes ${agentBeliefs[agent].formulas.join(' and ')} and k(${agent}) = ${agentBeliefs[agent].denotation}<br>`;
+        outputText += `${coloredAgentName} believes ${agentBeliefs[agent].messages.join(' and ')} and k(${agent}) = ${agentBeliefs[agent].denotation}<br>`;
     }
     // Using innerHTML instead of innerText since we are adding HTML tags
     document.getElementById("beliefOutput").innerHTML = outputText;
 }
 
 
+/*for verification  
+
+
+(p+(~q&((r+~p)>~(p>~r)))) ===> {{p}, {q, p}, {r, p}, {r, q, p}}
+
+((p > q) & (q > r)) ===>  {{}, {r}, {r, q}, {r, q, p}}
+
+both ===> {{r, q, p}}
+*/
 
 
 // ================================================
 // =============== AUXILIARY FUNCTIONS ============
 // ================================================
 
-// Remove the redundant arraysAreEqual inside complementOfSet
+// Global arraysAreEqual function
+function arraysAreEqual(arr1, arr2) {
+    if (arr1.length !== arr2.length) return false;
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) return false;
+    }
+    return true;
+}
+
+
 function setUnion(setA, setB) {
     const union = [...setA];
     for (const subset of setB) {
@@ -320,16 +339,6 @@ function setUnion(setA, setB) {
 function setIntersection(setA, setB) {
     return setA.filter(subsetA => setB.some(subsetB => arraysAreEqual(subsetA, subsetB)));
 }
-
-function arraysAreEqual(arr1, arr2) {
-    if (arr1.length !== arr2.length) return false;
-    for (let i = 0; i < arr1.length; i++) {
-        if (arr1[i] !== arr2[i]) return false;
-    }
-    return true;
-}
-
-
 
 function complementOfSet(set) {
     let powerSetOfProp = powerSet(Prop);
@@ -352,25 +361,172 @@ function powerSet(array) {
     return array.reduce((subsets, value) => subsets.concat(subsets.map(set => [value, ...set])), [[]]);
 }
 
-// Global arraysAreEqual function
-function arraysAreEqual(arr1, arr2) {
-    if (arr1.length !== arr2.length) return false;
-    for (let i = 0; i < arr1.length; i++) {
-        if (arr1[i] !== arr2[i]) return false;
+
+
+function isSubsetOf(subset, superset) {
+    for (let element of subset) {
+        if (!superset.includes(element)) {
+            return false;
+        }
     }
     return true;
+}
+
+function parseDenotationString(denotationString) {
+    return denotationString
+        .slice(2, -2)  // Remove the outer '{{' and '}}'
+        .split('}, {')  // Split by '}, {' to get individual worlds
+        .map(worldString => 
+            worldString.split(', ').map(atom => atom.slice(1, -1))  // Split by ', ' and remove the surrounding '{' and '}'
+        );
+}
+
+// Sample usage:
+// const denotationString = '{{p}, {q, p}, {r, p}, {r, q, p}}';
+// console.log(parseDenotationString(denotationString));
+// Expected output: [['p'], ['q', 'p'], ['r', 'p'], ['r', 'q', 'p']]
+
+
+//evaluate formula
+
+function satisfiability() {
+    const formula = document.getElementById("formulaInput").value.trim();
+    const subtokens = tokenizeFormula(formula); // Get tokens first
+    const parsedFormula = parseFormula(subtokens); // Pass tokens to the parser
+    
+    const satResult = checkSatisfiability(parsedFormula);
+
+    document.getElementById("satisfaction").innerText = satResult ? "The formula is satisfiable." : "The formula is not satisfiable.";
+}
+
+
+function tokenizeFormula(formula) {
+    if (typeof formula !== "string") {
+        throw new TypeError("Formula must be a string.");
+    }
+
+    // The order matters; we want to capture multi-character tokens before single ones.
+    return formula.match(/B[a-z]|~|&|\+|\[.*?\]|[a-z]_[0-9]+|[a-z]|[\(\)]/g);
+}
+
+
+function parseFormula(subtokens) {
+    console.log("Parsing formula with subtokens:", subtokens);
+
+    let stack = [];
+
+    for (let [index, token] of subtokens.entries()) {
+        if (token.startsWith('B')) {
+            let agent = token[1];  // Since the token is like 'Ba', the agent is the second character
+            let remainingTokens = subtokens.slice(index + 1); // get the remaining tokens
+            let proposition = parseFormula(remainingTokens); // recursively parse the remaining tokens
+            stack.push({ type: "belief", agent: agent, proposition: proposition });
+        } else {
+           
+            switch (token) {
+                case '~':
+                    let negatedElement = getNextToken(subtokens);
+                    stack.push({ type: "not", element: negatedElement });
+                    break;
+                case '&':
+                    let leftConjunct = stack.pop();
+                    let rightConjunct = getNextToken(subtokens);
+                    stack.push({ type: "and", left: leftConjunct, right: rightConjunct });
+                    break;
+                case '+':
+                    let leftDisjunct = stack.pop();
+                    let rightDisjunct = getNextToken(subtokens);
+                    stack.push({ type: "or", left: leftDisjunct, right: rightDisjunct });
+                    break;
+                case '[':
+                    let announcementAgent = getNextToken(subtokens);
+                    let announcementProposition = getNextToken(subtokens);
+                    stack.push({ type: "announcement", agent: announcementAgent, proposition: announcementProposition });
+                    break;
+                default:
+                    if (token.match(/^[a-z](_[0-9]+)?$/)) {  // Checks if token is an atomic proposition
+                        stack.push({ type: 'atom', value: token });
+                    } else {
+                        throw new Error(`Unexpected token: ${token} at position ${index} in formula.`);
+                    }
+                    break;
+            }
+        }
+    }
+
+    return stack[0];
+}
+
+function includesArray(bigArray, smallArray) {
+    return bigArray.some(arr => JSON.stringify(arr) === JSON.stringify(smallArray));
 }
 
 
 
 
 
-/*for verification  
+function getNextToken(subtokens) {
+    return subtokens.shift();
+}
+
+function checkSatisfiability(parsedFormula) {
+    switch(parsedFormula.type) {
+        case "belief":
+            const agent = parsedFormula.agent;
+            const proposition = parsedFormula.proposition;
+
+            if (!agentBeliefs[agent]) {
+                console.error(`Agent '${agent}' does not have any assigned beliefs.`);
+                return false; 
+            }
+            console.log(`Beliefs for agent ${agent}:`, agentBeliefs[agent]);
+
+            if (typeof agentBeliefs[agent].denotation === 'string') {
+                agentBeliefs[agent].denotation = parseDenotationString(agentBeliefs[agent].denotation);
+            }
+
+            const agentBeliefWorlds = agentBeliefs[agent] && agentBeliefs[agent].denotation;
+            console.log("agentBeliefWorlds:", agentBeliefWorlds);
+            const denotation_to_array = replaceWithDenotation(proposition)
+            const propositionDenotation = parseDenotationString(denotation_to_array);
+            console.log("propositionDenotation:", propositionDenotation);
+            
+            if (!Array.isArray(agentBeliefWorlds) || !agentBeliefWorlds.every(Array.isArray)) {
+                console.error(`AgentBeliefWorlds for agent '${agent}' is not a proper array of arrays.`);
+                return false;
+            }
+
+            if (!Array.isArray(propositionDenotation) || !propositionDenotation.every(Array.isArray)) {
+                console.error("Proposition denotation is not a proper array of arrays:", propositionDenotation);
+                return false;
+            }
+
+            return agentBeliefWorlds.every(beliefWorld => includesArray(propositionDenotation, beliefWorld));
+       
+        case "not":
+            return !checkSatisfiability(parsedFormula.element);
+
+        case "and":
+            return checkSatisfiability(parsedFormula.left) && checkSatisfiability(parsedFormula.right);
+
+        case "or":
+            return checkSatisfiability(parsedFormula.left) || checkSatisfiability(parsedFormula.right);
+
+        case "announcement":
+            // Assuming public announcement; for now, we'll just return true.
+            // More advanced handling for public announcement can be added later.
+            return true;
+
+        default:
+            console.error(`Unknown formula type: ${parsedFormula.type}`);
+            return false;
+    }
+}
 
 
-(p+(~q&((r+~p)>~(p>~r)))) ===> {{p}, {q, p}, {r, p}, {r, q, p}}
 
-((p > q) & (q > r)) ===>  {{}, {r}, {r, q}, {r, q, p}}
 
-both ===> {{r, q, p}}
-*/
+
+
+
+
